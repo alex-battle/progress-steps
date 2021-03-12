1a5b5521b40ca15d41444c38d8dc9aa8120222e7
Alex Battle <58659536+alex-battle@users.noreply.github.com>
2021-03-12 10:31:46 -0500

    progress bar loading

--- script.js
+++ script.js
@@ -32,4 +32,8 @@ function update(){
             circle.classList.remove('active')
         }
     })
+
+    const actives = document.querySelectorAll('.active')
+
+    progress.style.width = ((actives.length -1) / (circles.length -1)) * 100 + '%'
     }
